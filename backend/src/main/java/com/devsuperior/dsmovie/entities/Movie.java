ca4696f9568f209a6fde60830638054a58711bd1
Sophia Fiama <fiama.sophia.sf@gmail.com>
2022-01-15 15:24:01 -0300

    First homolog

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -22,7 +22,7 @@ public class Movie {
 	private Integer count; 
 	private String image; 
 	
-	@OneToMany(mappedBy = "id.movie")
+	@OneToMany(mappedBy="id.movie")
 	private Set<Score> scores = new HashSet<>(); 
 	
 	public Movie() {
