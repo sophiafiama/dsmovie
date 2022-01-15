a8d90175248e276a363f381e8a2ecda8e8bded51
Sophia Fiama <fiama.sophia.sf@gmail.com>
2022-01-15 16:36:24 -0300

    update front

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -18,7 +18,7 @@ export default function MovieCard() {
                 <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
                 <div className="dsmovie-card-bottom-container">
                     <h3>{movie.title}</h3>
-                    <MovieScore />
+                    <MovieScore/>
 
                     <Link to={`/form/${movie.id}`}>
                         <div className="btn btn-primary dsmovie-btn">Avaliar</div>
