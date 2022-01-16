85bb470fe1f904f486a32626b48e8041645c6800
Sophia Fiama <fiama.sophia.sf@gmail.com>
2022-01-16 18:26:10 -0300

    Update front

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as StarFull } from 'assets/img/star-full.svg';
 import { ReactComponent as StarHalf } from 'assets/img/star-half.svg';
 import { ReactComponent as StarEmpty } from 'assets/img/star-empt.svg';
 import './styles.css'; 
-import { type } from 'os';
+
 
 type Props={
     score:number;
